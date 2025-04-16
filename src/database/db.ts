4e60bc89a8a2c121c0dda1e208b8231dcb6f1ced
Herman2201 <ushkalo.herman@gmail.com>
2025-04-16 14:18:14 +0300

    log database configuration on initialization

--- src/database/db.ts
+++ src/database/db.ts
@@ -3,3 +3,4 @@ import knexConfig from '../../knexfile';
 import { configuration } from '../config';
 
 export const db = Knex(knexConfig[configuration.server.nodeEnvironment]);
+console.log(knexConfig[configuration.server.nodeEnvironment]);
